82f901275fb06bffa1b94e855cfa6f28d55f7dd2
jcarlo <janta1696@gmail.com>
2021-10-30 06:54:14 -0600

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,35 @@ const ELEMENT_DATA = [
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-material-file-upload-app';
-  displayedColumns: string[] = ['estado', 'estadoUno', 'estadoDos'];
-
+  
+  displayedColumns: string[] = [
+    'estado',
+    'estadoUno',
+    'estadoDos',
+    'estadoTres',
+  ];
+  inputModel = '';
+  displayedColumnsComp: string[] = [
+    'estado',
+    'estadoA',
+    'estadoB',
+    'composicion',
+  ];
+  errorInput = true;
   dataSource = [];
+  dataSourceComp = [];
   linesContent = [];
   linesVertical = [];
+  linesContentDeterminista = [];
+  compocisiones = [];
+  estadosCompociones: Array<any> = [];
 
   @ViewChild('UploadFileInput') uploadFileInput: ElementRef;
   myfilename = 'Seleccionar archivo';
 
-  constructor() {}
+  constructor() {
+    
+  }
 
   fileChangeEvent(fileInput: any) {
     if (fileInput.target.files && fileInput.target.files[0]) {
@@ -75,8 +93,9 @@ export class AppComponent {
           content = content.replaceAll(':', '');
           content = content.replaceAll('}', '');
           content = content.replaceAll(' ', '');
-          content = content.replaceAll('(', '');
+          content   = content.replaceAll('(', '');
           content = content.replaceAll(')', '');
+          let contentMatriz = content.split(';');
           content = content.replaceAll(',', '');
           let linesContent = content.split(';');
           console.log('linesContent', linesContent);
@@ -107,11 +126,33 @@ export class AppComponent {
             }
           }
           console.log('estadosList', estadosList);
-          this.dataSource = estadosList.map((e) => ({
-            estado: e.estado,
-            estadoUno: e.redirecciones[0].redireccion,
-            estadoDos: e.redirecciones[1].redireccion,
-          }));
+          this.dataSource = estadosList.map((e) => {
+            let obj = {
+              estado: e.estado,
+              estadoUno: '',
+              estadoDos: '',
+              estadoTres: '',
+            };
+            for (let i = 0; i < e.redirecciones.length; i++) {
+              switch (e.redirecciones[i].llave) {
+                case 'a': {
+                  obj.estadoUno = e.redirecciones[i].redireccion;
+                  break;
+                }
+                case 'b': {
+                  obj.estadoDos = e.redirecciones[i].redireccion;
+                  break;
+                }
+                case 'e': {
+                  obj.estadoTres = e.redirecciones[i].redireccion;
+                  break;
+                }
+              }
+            }
+
+            return obj;
+          });
+          this.printAFD(contentMatriz);
           break;
         }
         default: {
@@ -121,4 +162,147 @@ export class AppComponent {
 
     this.linesVertical = linesVe;
   }
+
+  printAFD(contentMatriz:any) {
+    const matriz = contentMatriz.map(e => e.split(','));
+    const abs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+    let estado = matriz[0][0];
+    let composicion = estado;
+
+    for (let i = 0; i < matriz.length; i++) {
+      if (estado == matriz[i][0] && matriz[i][1] == 'e') {
+        composicion += matriz[i][2];
+        estado = matriz[i][2];
+      }
+    }
+
+    const nuevaComposicion = {
+      estado: abs.shift(),
+      composicion: composicion,
+      read: false,
+    };
+    let composiciones: Array<any> = [nuevaComposicion];
+    
+    for (let i = 0; i < composiciones.length; i++) {
+      if (!composiciones[i].read) {
+        let obj = {
+          estado: composiciones[i].estado,
+          composicion: composiciones[i].composicion,
+          estadoA: '',
+          estadoB: '',
+        };
+        let compociA = this.encontrarComposicion(
+          composiciones[i].composicion,
+          'a',
+          matriz
+        );
+        let existCompA = this.existeLaComposicion(compociA, composiciones);
+        if (existCompA) {
+          obj.estadoA = existCompA;
+        } else {
+          let estado = abs.shift();
+          composiciones.push({
+            estado: estado,
+            composicion: compociA ?  compociA : '',
+          });
+          obj.estadoA = estado;
+        }
+        let compociB = this.encontrarComposicion(
+          composiciones[i].composicion,
+          'b',
+          matriz
+        );
+        let existCompB = this.existeLaComposicion(compociB, composiciones);
+        if (existCompB) {
+          obj.estadoB = existCompB;
+        } else {
+          let estado = abs.shift();
+          composiciones.push({
+            estado: estado,
+            composicion: compociB ? compociB : '',
+          });
+          obj.estadoB = estado;
+        }
+
+        this.estadosCompociones.push(obj);
+        composiciones[i].read = true;
+        // this.markComposicion(composiciones[i].composicion.estado);
+      }
+    }
+    // this.compocisiones.push(composicion);
+    // console.log("compocisiones",this.compocisiones);
+
+    this.dataSourceComp = this.estadosCompociones.map(e => ({...e, composicion: e.composicion ? e.composicion.split("").join(',') : '0'}));
+    console.log('compocisiones', composiciones);
+    console.log('estados', this.estadosCompociones);
+    this.generarAutomataDeterministaFinito(this.estadosCompociones);
+  }
+
+  generarAutomataDeterministaFinito(estadosCompociones ) {
+    try {
+      let Q = `Q: {${estadosCompociones.map(e => e.estado).join(',')}}`;
+      let F = `F: {a, b} `;
+      let I = `i: A`;
+      let A = `A: {${estadosCompociones.filter(e => e.composicion).map(e => e.estado).join(",")}}`
+      this.linesContentDeterminista = [Q , F , I , A];
+    } catch (err) {
+      console.log('err generarAutomataDeterministaFinito', err);
+    }
+
+  }
+
+  encontrarComposicion(composicion, letra, matriz) {
+    let nuevaComposicion = '';
+
+    let ultipoEstado = false;
+    for (let i = 0; i < matriz.length; i++) {
+      if (composicion.includes(matriz[i][0]) && matriz[i][1] == letra) {
+        ultipoEstado = true;
+        if (!nuevaComposicion.includes(matriz[i][2])) {
+          nuevaComposicion += matriz[i][2];
+        }
+      }
+      if (
+        ultipoEstado &&
+        composicion.includes(matriz[i][0]) &&
+        matriz[i][1] == 'e'
+      ) {
+        ultipoEstado = false;
+        if (!nuevaComposicion.includes(matriz[i][2])) {
+          nuevaComposicion += matriz[i][2];
+        }
+      }
+    }
+    return nuevaComposicion;
+  }
+
+  existeLaComposicion(composicion, composiciones): any {
+    let estadoCompo = '';
+    for (let i = 0; i < composiciones.length; i++) {
+      if (composiciones[i].composicion == composicion) {
+        estadoCompo = composiciones[i].estado;
+      }
+    }
+    return estadoCompo;
+  }
+
+  markComposicion(estado) {
+    let constComp = this.compocisiones.map((composicion) => {
+      if (composicion.estado == estado) {
+        composicion.read = true;
+      }
+    });
+    this.compocisiones = constComp;
+  }
+
+  changeInputModel(event) {
+    console.log('event',event);
+  }
+
+  validCadena(cadena) {
+
+  }
+
+  
 }
